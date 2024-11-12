f01cf5c1f35d40fe9cbeff922ae139d4ff376bd0
kdon5chima <kdonchima@gmail.com>
2024-11-12 06:10:43 -0800

    First commit for version control 11/11/2024

---
+++ index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Welcome to Unique Group of Schools...</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <style>
+        body{
+           background-image: url(images/log1.png);
+            
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="assets/custom/cover.css" rel="stylesheet">
+
+  </head>
+  <body>
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <img src="images/logo.png" alt="">
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+      <button type="button" class="btn btn-outline-success">
+        <a class="nav-link text-danger" href="#">Unique Children School</a>
+      </button>
+      <button type="button" class="btn btn-outline-warning">
+        <a class="nav-link text-danger" href="#">Unique Heights School</a>
+      </button>
+      </nav>
+    </div>
+  </header>
+ <br /><br /><br /><br />
+  <main class="px-100">
+    <h2 class="text-white">Welcome to Unique Group of Schools<h2>
+    <br /><br />
+    <center><h3 class="text-white">Site under reconstruction....<h3></center>
+    <br /><br />
+    <table>
+      <tr>
+        <td width="360"> 
+         
+        <button type="button" class="text-white bg-dark">
+            <a href="#" class="text-white text-decoration-none"><h6>Click to visit <br />Unique Children School</h6></a>
+        </button>
+        </td>
+        <td width="100"></td>
+        <td width="360">
+        <button type="button" class="text-white bg-danger">
+              <a href="#" class="text-white text-decoration-none"><h6>Click to visit <br />Unique Height School</h6></a>
+        </button>
+        </td>
+      </tr>
+    </table>
+      <div>
+     
+  </div>
+  </main>
+  <br /><br /><br /><br />
+  <footer class="mt-auto text-white-50">
+    <!-- footer -->
+    <p>Unique Group of Schools...</p>
+  </footer>
+</div>
+</body>
+</html>
