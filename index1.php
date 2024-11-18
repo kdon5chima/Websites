265358eeded367650298ff184e41b2fdf0ebcb48
kdon5chima <kdonchima@gmail.com>
2024-11-18 08:33:14 -0800

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Welcome to Unique Group of Schools...</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body{
+           background-image: url(images/log1.png);
+            
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+            background-size: cover;
+            width: 100%;
+            height: 400px
+            
+        }
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+      h3{
+        position: center;
+        font-size: 36px;
+        color: blue;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+        @media only screen and (max-width:800px) {
+  /* For tablets: */
+  .main {
+    width: 80%;
+    padding: 0;
+  }
+  .right {
+    width: 100%;
+  }
+}
+@media only screen and (max-width:500px) {
+  /* For mobile phones: */
+  .menu, .main, .right {
+    width: 100%;
+  }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="assets/custom/cover.css" rel="stylesheet">
+
+  </head>
+  <body>
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <img src="images/logo.png" alt="">
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+      <button type="button" class="btn btn-outline-success">
+        <a class="nav-link text-danger" href="ucs/index.php">Unique Children School</a>
+      </button>
+      <button type="button" class="btn btn-outline-warning">
+        <a class="nav-link text-danger" href="uhs/index.php">Unique Heights School</a>
+      </button>
+      </nav>
+    </div>
+  </header>
+ <br /><br /><br /><br />
+  <main class="px-100">
+    
+    <br /><br />
+    <h3 class="text-white">Site under reconstruction....</h3>
+    <br /><br />
+   
+      <div>
+     
+  </div>
+  </main>
+  <br /><br /><br /><br />
+  <footer class="mt-auto text-white-50">
+
+  </footer>
+</div>
+</body>
+</html>
